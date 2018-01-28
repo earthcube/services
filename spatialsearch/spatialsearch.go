c398720572e1940be6774cc8e62b7089f6d97c06
Douglas Fils <drfils@gmail.com>
2018-01-28 11:18:45 -0600

    finally resolved gejson approach that doesn't SUCK

--- spatialsearch/spatialsearch.go
+++ spatialsearch/spatialsearch.go
@@ -2,15 +2,12 @@ package spatialsearch
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
-	"strconv"
-	"strings"
 
 	"github.com/emicklei/go-restful"
 	"github.com/garyburd/redigo/redis"
-	gj "github.com/kpawlik/geojson"
+	"github.com/paulmach/go.geojson"
 )
 
 // LocType is there to do a first cut marshalling to just get the type before  next marshalling
@@ -18,23 +15,45 @@ type LocType struct {
 	Type string `json:"type"`
 }
 
-// LocationPoint is a simple type and cooridnates struct for schema.org spatial info
-type LocationPoint struct {
-	Type        string    `json:"type"`
-	Coordinates []float64 `json:"coordinates"`
-}
+// // LocationPoint is a simple type and cooridnates struct for schema.org spatial info
+// type LocationPoint struct {
+// 	Type        string    `json:"type"`
+// 	Coordinates []float64 `json:"coordinates"`
+// }
 
-// LocationPoly is a simple type and cooridnates struct for schema.org spatial info
-type LocationPoly struct {
-	Type        string        `json:"type"`
-	Coordinates [][][]float64 `json:"coordinates"`
-}
+// // LocationPoly is a simple type and cooridnates struct for schema.org spatial info
+// type LocationPoly struct {
+// 	Type        string        `json:"type"`
+// 	Coordinates [][][]float64 `json:"coordinates"`
+// }
 
-// LocationFeature is a simple type and cooridnates struct for schema.org spatial info
-type LocationFeature struct {
-	Type     string       `json:"type"`
-	Geometry LocationPoly `json:"geometry"`
-}
+// // LocationFeature is a simple type and cooridnates struct for schema.org spatial info
+// type LocationFeature struct {
+// 	Type     string       `json:"type"`
+// 	Geometry LocationPoly `json:"geometry"`
+// }
+
+// // GeoJOSN struct
+// type GeoJSON struct {
+// 	Type     string        `json:"type"`
+// 	Features []GeoFeatures `json:"features"`
+// }
+
+// // GeoFeatures
+// type GeoFeatures struct {
+// 	Type       string        `json:"type"`
+// 	Properties GeoProperties `json:"properties"`
+// 	Geometry   GeoGeometry   `json:"geometry"`
+// }
+
+// type GeoProperties struct {
+// 	URL string `json:URL`
+// }
+
+// type GeoGeometry struct {
+// 	Type        string    `json:"type"`
+// 	Coordinates []float64 `json:"coordinates"`
+// }
 
 // New builds out the services calls..
 func New() *restful.WebService {
@@ -66,12 +85,9 @@ func SpatialCall(request *restful.Request, response *restful.Response) {
 	}
 	defer c.Close()
 
-	log.Print("connected")
-
-	var value1 int // we seem to be getting RESP back..
+	var value1 int
 	var value2 []interface{}
 	reply, err := redis.Values(c.Do("INTERSECTS", "p418", "LIMIT", "50000", "OBJECT", wktstring))
-	// reply, err := redis.String(c.Do("INTERSECTS", "p418", "LIMIT", "50000", "OBJECT", wktstring))
 	// reply, err := redis.Values(c.Do("SCAN", "p418"))
 	if err != nil {
 		fmt.Printf("Error in reply %v \n", err)
@@ -82,31 +98,20 @@ func SpatialCall(request *restful.Request, response *restful.Response) {
 
 	fmt.Println(value1)
 
-
-	// results, _ := tile38RespAsGeoJSON(value2)
-	// response.Write([]byte(results))
-	response.Write([]byte("Hi there"))
+	results, _ := templateTest(value2)
+	response.Write([]byte(results))
 }
 
-// this is a a lot of pointless work!
-func tile38RespAsGeoJSON(results []interface{}) (string, error) {
+func templateTest(results []interface{}) (string, error) {
 
-	// Build the geojson section
-	var (
-		// fc *gj.FeatureCollection
-		f  *gj.Feature
-		fa []*gj.Feature
-	)
+	fc := geojson.NewFeatureCollection()
 
 	for _, item := range results {
 		valcast := item.([]interface{})
 		val0 := fmt.Sprintf("%s", valcast[0])
 		val1 := fmt.Sprintf("%s", valcast[1])
 
-		log.Printf("%s  %s", val0, val1)
-
-		// serialize val1 to a simple struct with just type
-		// then move on to the next level of serilization seen below
+		// log.Printf("%s %s \n", val0, val1)
 
 		lt := &LocType{}
 		err := json.Unmarshal([]byte(val1), lt)
@@ -115,125 +120,43 @@ func tile38RespAsGeoJSON(results []interface{}) (string, error) {
 			return "", err
 		}
 
-		if lt.Type == "Point" {
-			lp := &LocationPoint{}
-			err := json.Unmarshal([]byte(val1), lp)
-			if err != nil {
-				log.Print(err)
-				return "", err
-			}
-			if lp.Type == "Point" {
-				log.Println("add point")
-				cd := gj.Coordinate{gj.Coord(lp.Coordinates[0]), gj.Coord(lp.Coordinates[1])} // is this long lat..  vs lat long?
-				props := map[string]interface{}{"URL": val0}
-				newp := gj.NewPoint(cd)
-				f = gj.NewFeature(newp, props, nil)
-				fa = append(fa, f)
-			}
-		}
+		rawGeometryJSON := []byte(val1)
 
-		// ugh..  must I deconstruct and restructure what I know if valid GeoJSON?
-		// Can I always ensure my spatial index is made of geojson..  since we build it..
-		if lt.Type == "Polygon" {
-			log.Println("add poly")
-			lp := &LocationPoly{}
-			err := json.Unmarshal([]byte(val1), lp)
+		if lt.Type == "Point" || lt.Type == "Poly" {
+			g, err := geojson.UnmarshalGeometry(rawGeometryJSON)
 			if err != nil {
-				log.Print(err)
-				return "", err
+				log.Printf("Unmarshal geom error for %s with %s\n", val0, err)
 			}
-			log.Print(lp)
+
+			switch {
+			case g.IsPoint():
+				nf := geojson.NewFeature(g)
+				nf.SetProperty("URL", val0)
+				fc.AddFeature(nf)
+			case g.IsPolygon():
+				nf := geojson.NewFeature(g)
+				nf.SetProperty("URL", val0)
+				fc.AddFeature(nf)
+			default:
+				log.Println(g.Type)
+			}
 		}
 
 		if lt.Type == "Feature" {
-			log.Println("add feature")
-			lf := &LocationFeature{}
-			err := json.Unmarshal([]byte(val1), lf)
+			f, err := geojson.UnmarshalFeature(rawGeometryJSON)
 			if err != nil {
-				log.Print(err)
-				return "", err
+				log.Printf("Unmarshal feature error for %s with %s\n", val0, err)
 			}
-			log.Print(lf)
+			f.SetProperty("URL", val0)
+			fc.AddFeature(f)
 		}
 
 	}
 
-	fc := gj.FeatureCollection{Type: "FeatureCollection", Features: fa}
-	gjstr, err := gj.Marshal(fc)
+	rawJSON, err := fc.MarshalJSON()
 	if err != nil {
-		log.Println(err)
+		return "", err
 	}
 
-	// log.Println(gjstr)
-
-	return gjstr, nil
-}
-
-// WKTPolygonToFloatArray return float64 array for WKT Poly string
-func WKTPolygonToFloatArray(wkt string) ([][][]float64, error) {
-	// Take WKT string and parse down
-	parsedString := strings.TrimSuffix(strings.TrimPrefix(wkt, "POLYGON(("), "))")
-	wktarray := strings.Split(parsedString, ",")
-
-	f := [][][]float64{}
-	c := [][]float64{}
-
-	for _, item := range wktarray {
-		coordSet := strings.Split(item, " ")
-		// TODO..  catch these errors..  this is bad form!  The whole function needs an error
-		x, err := strconv.ParseFloat(coordSet[0], 64)
-		y, err := strconv.ParseFloat(coordSet[1], 64)
-		cd := []float64{x, y}
-		c = append(c, cd)
-		if err != nil {
-			log.Println(err)
-			return f, errors.New("Error in the conversion of WKT to GeoJSON Polygon to support spatial search")
-		}
-	}
-
-	f = append(f, c)
-
-	fmt.Println(f)
-	return f, nil
-}
-
-// WKTPolygontoGeoJSON convert WKT to GeoJSON for Polygons.
-// Ended up not using this in ocdService since mgo driver needed a bson structure for query
-func WKTPolygontoGeoJSON(wkt string) string {
-	var (
-		// fc *gj.FeatureCollection
-		//f  *gj.Feature
-		//fa []*gj.Feature
-		newp *gj.Polygon
-	)
-
-	// Take WKT string and parse down
-	parsedString := strings.TrimSuffix(strings.TrimPrefix(wkt, "POLYGON(("), "))")
-	wktarray := strings.Split(parsedString, ",")
-
-	c := gj.Coordinates{}
-	for _, item := range wktarray {
-		coordSet := strings.Split(item, " ")
-		// TODO..  catch these errors..  this is bad form!  The whole function needs an error
-		x, _ := strconv.ParseFloat(coordSet[0], 64)
-		y, _ := strconv.ParseFloat(coordSet[1], 64)
-		cd := gj.Coordinate{gj.Coord(x), gj.Coord(y)}
-		c = append(c, cd)
-
-	}
-	newml := gj.MultiLine{c}
-	newp = gj.NewPolygon(newml)
-	//f = gj.NewFeature(newp, nil, nil)
-	//fa = append(fa, f)
-
-	//fc := gj.FeatureCollection{Type: "FeatureCollection", Features: fa}
-	gjstr, err := gj.Marshal(newp)
-	if err != nil {
-		//panic(err)
-		log.Printf("Error event: %v \n", err)
-	}
-
-	fmt.Println(gjstr)
-
-	return gjstr
+	return string(rawJSON), nil
 }
--- testClientSpatial.py
+++ testClientSpatial.py
@@ -4,6 +4,14 @@ import requests
 URL = 'http://localhost:6789/api/v1/spatial/search/test1'
 # URL = 'http://geodex.org/api/v1/spatial/search/test1'
 
+
+# http://get.iedadata.org/doi/315201  
+# {"type":"Polygon","coordinates":[[[-16.91266,28.11008],[-16.91266,33.31179],[-9.24511,33.31179],[-9.24511,28.11008],[-16.91266,28.11008]]]}
+# http://opencoredata.org/id/dataset/273af50d-bffe-4094-9c0a-cd2f900b5474  
+# {"type":"Point","coordinates":[-21.09,11.82]}
+#  https://www.bco-dmo.org/dataset/628710  
+# {"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[-76.3573,34.98],[-76.3573,34.98],[-76.3573,34.98],[-76.3573,34.98],[-76.3573,34.98]]]}}
+
 data='''{
   "type": "FeatureCollection",
   "features": [
@@ -45,4 +53,4 @@ data='''{
 PARAMS = {'geowithin':data}
 r = requests.get(url = URL, params = PARAMS)
 print(r.content)
-#display(r.content)  # calls to geojson.io and opens your browser to view it..  
+# display(r.content)  # calls to geojson.io and opens your browser to view it..  
